1de1858c750d44d3629a207cfa3db9a4a67052f8
Nolan.C <62423743+N0lanC@users.noreply.github.com>
2021-03-23 13:26:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 int main(int argc, char *argv[]) {
     auto *app = new QApplication(argc, argv);
     auto myObject = new presenter(app);
-    //myObject->TestConnection();
 
     return QApplication::exec();
 }
